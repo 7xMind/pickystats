76bfbc70673da1f5ba9fc2f7de225a8f886519c1
Amir Afianian <a.afianian@aut.ac.ir>
2021-09-21 00:02:47 +0430

    reorder imports

--- pickystats/api/views.py
+++ pickystats/api/views.py
@@ -1,11 +1,11 @@
+import logging
+
 from django.utils.decorators import method_decorator
 from drf_yasg.openapi import Parameter
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-import logging
 
 from ..analyzer import PickyStats
 from .serializers import FileUploadSerializer
